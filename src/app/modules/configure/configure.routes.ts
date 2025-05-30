7bebb3f401aa84336d0d80e060f50e271c6a02a8
safimiazi <mohibullamiazi@gmail.com>
2025-05-30 16:46:03 +0600

    feat: add authentication middleware to pricing and subscription routes

--- src/app/modules/configure/configure.routes.ts
+++ src/app/modules/configure/configure.routes.ts
@@ -1,11 +1,13 @@
 
 import express from "express";
 import { configureController } from "./configure.controller";
+import USER_ROLE from "../../constants/userRole";
+import auth from "../../middleWear/auth";
 
 const router = express.Router();
 
-router.get("/get-configure", configureController.getAllConfigure);
-router.get("/get_single_configure/:id", configureController.getSingleConfigure);
-router.put("/update-configure/:id", configureController.updateConfigure);
-router.delete("/delete-configure/:id", configureController.deleteConfigure);
+router.get("/get-configure", auth(USER_ROLE.ADMIN), configureController.getAllConfigure);
+router.get("/get_single_configure/:id", auth(USER_ROLE.ADMIN), configureController.getSingleConfigure);
+router.put("/update-configure/:id", auth(USER_ROLE.ADMIN), configureController.updateConfigure);
+router.delete("/delete-configure/:id", auth(USER_ROLE.ADMIN), configureController.deleteConfigure);
 export const configureRoutes = router;
--- src/app/modules/subscription/subscription.routes.ts
+++ src/app/modules/subscription/subscription.routes.ts
@@ -5,15 +5,15 @@ import {
   createCheckoutSession,
   getSubscriptionStatus,
 } from "./subscription.controller";
+import USER_ROLE from "../../constants/userRole";
+import auth from "../../middleWear/auth";
 
 const router = express.Router();
 
 
-router.post("/create-checkout-session", createCheckoutSession);
+router.post("/create-checkout-session",  auth(USER_ROLE.USER), createCheckoutSession);
 router.post("/cancel-subscription", cancelSubscription);
-router.get("/premium-data", checkSubscription, (req, res) => {
-  res.json({ message: "Welcome to premium features!" });
-});
+
 router.get("/subscription-status", getSubscriptionStatus);
 
 
