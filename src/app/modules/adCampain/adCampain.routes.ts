9c693eafef880c748a0f67dbdde7358063abe321
safimiazi <mohibullamiazi@email.com>
2025-07-07 16:54:20 +0600

    Udpate

--- src/app/modules/adCampain/adCampain.routes.ts
+++ src/app/modules/adCampain/adCampain.routes.ts
@@ -1,20 +1,17 @@
 
 import express from "express";
 import { adCampainController } from "./adCampain.controller";
-
-import { adCampainPostValidation, adCampainUpdateValidation } from "./adCampain.validation";
-import { validateRequest } from "../../middleWear/validateRequest";
 import auth from "../../middleWear/auth";
 import USER_ROLE from "../../constants/userRole";
 
 const router = express.Router();
 
-router.post("/post-ads-campaign", adCampainController.postAdCampain);
-router.get("/get-all-ads-campaign", adCampainController.getAllAdCampain);
-router.get("/get-single-ads-campaign/:id", adCampainController.getSingleAdCampain);
+router.post("/post-ads-campaign",auth(USER_ROLE.ADMIN, USER_ROLE.CREATOR), adCampainController.postAdCampain);
+router.get("/get-all-ads-campaign",auth(USER_ROLE.ADMIN, USER_ROLE.CREATOR, USER_ROLE.VIEWER), adCampainController.getAllAdCampain);
+router.get("/get-single-ads-campaign/:id",auth(USER_ROLE.ADMIN, USER_ROLE.CREATOR, USER_ROLE.VIEWER), adCampainController.getSingleAdCampain);
 router.get('/dashboard-summary', auth(USER_ROLE.ADMIN, USER_ROLE.CREATOR, USER_ROLE.VIEWER), adCampainController.getAdCampainsInfo)
-router.put("/update-ads-campaign/:id", adCampainController.updateAdCampain);
-router.delete("/delete-ads-campaign/:id", adCampainController.deleteAdCampain);
+router.put("/update-ads-campaign/:id",auth(USER_ROLE.ADMIN, USER_ROLE.CREATOR), adCampainController.updateAdCampain);
+router.delete("/delete-ads-campaign/:id",auth(USER_ROLE.ADMIN, USER_ROLE.CREATOR), adCampainController.deleteAdCampain);
 
 
 export const adCampainRoutes = router;
