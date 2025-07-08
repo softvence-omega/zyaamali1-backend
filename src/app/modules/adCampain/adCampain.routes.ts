0852d277f0987884dd208e25e0c97d2826d2c3f6
Humayun Kabir <humayunkabir@bdcallings-Mac-mini-5.local>
2025-07-08 08:39:29 +0600

    modify

--- src/app/modules/adCampain/adCampain.routes.ts
+++ src/app/modules/adCampain/adCampain.routes.ts
@@ -9,9 +9,9 @@ import USER_ROLE from "../../constants/userRole";
 
 const router = express.Router();
 
-router.post("/post-ads-campaign", adCampainController.postAdCampain);
-router.get("/get-all-ads-campaign", adCampainController.getAllAdCampain);
-router.get("/get-single-ads-campaign/:id", adCampainController.getSingleAdCampain);
+router.post("/post-ads-campaign",auth(USER_ROLE.ADMIN), adCampainController.postAdCampain);
+router.get("/get-all-ads-campaign",auth(USER_ROLE.ADMIN, USER_ROLE.CREATOR, USER_ROLE.VIEWER), adCampainController.getAllAdCampain);
+router.get("/get-single-ads-campaign/:id",auth(USER_ROLE.ADMIN, USER_ROLE.CREATOR, USER_ROLE.VIEWER), adCampainController.getSingleAdCampain);
 router.get('/dashboard-summary', auth(USER_ROLE.ADMIN, USER_ROLE.CREATOR, USER_ROLE.VIEWER), adCampainController.getAdCampainsInfo)
 router.put("/update-ads-campaign/:id", adCampainController.updateAdCampain);
 router.delete("/delete-ads-campaign/:id", adCampainController.deleteAdCampain);
