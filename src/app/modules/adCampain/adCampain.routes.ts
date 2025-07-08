a6abeb9f680333c124d4524d10246e77306f283c
Humayun Kabir <humayunkabir@bdcallings-Mac-mini-5.local>
2025-07-08 15:21:24 +0600

    adCampain logics modify

--- src/app/modules/adCampain/adCampain.routes.ts
+++ src/app/modules/adCampain/adCampain.routes.ts
@@ -12,6 +12,10 @@ const router = express.Router();
 router.post("/post-ads-campaign",auth(USER_ROLE.ADMIN), adCampainController.postAdCampain);
 router.get("/get-all-ads-campaign",auth(USER_ROLE.ADMIN, USER_ROLE.CREATOR, USER_ROLE.VIEWER), adCampainController.getAllAdCampain);
 router.get("/get-single-ads-campaign/:id",auth(USER_ROLE.ADMIN, USER_ROLE.CREATOR, USER_ROLE.VIEWER), adCampainController.getSingleAdCampain);
+router.get("/get-campaigns-table", auth(USER_ROLE.ADMIN, USER_ROLE.CREATOR, USER_ROLE.VIEWER), adCampainController.getActiveCampaignsTable)
+router.get("/get-active-campaigns", auth(USER_ROLE.ADMIN, USER_ROLE.CREATOR, USER_ROLE.VIEWER), adCampainController.getActiveCampaigns)
+router.get("/get-inactive-campaigns", auth(USER_ROLE.ADMIN, USER_ROLE.CREATOR, USER_ROLE.VIEWER), adCampainController.getInActiveCampaigns)
+router.get("/get-all-campaigns-performance", auth(USER_ROLE.ADMIN, USER_ROLE.CREATOR, USER_ROLE.VIEWER), adCampainController.getAlCampaignsPerformance)
 router.get('/dashboard-summary', auth(USER_ROLE.ADMIN, USER_ROLE.CREATOR, USER_ROLE.VIEWER), adCampainController.getAdCampainsInfo)
 router.put("/update-ads-campaign/:id", adCampainController.updateAdCampain);
 router.delete("/delete-ads-campaign/:id", adCampainController.deleteAdCampain);
