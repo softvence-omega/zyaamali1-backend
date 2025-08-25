4236b6e79247ec004ed100fe9941fa0b72d3a1a6
Md Soyaib Hossain <mdsoyaibhossain@macmini24.local>
2025-08-25 16:29:43 +0600

    Auto commit: Modified src/app.ts

--- src/app.ts
+++ src/app.ts
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(passport.initialize());
 
-router.get("/google/callback", googleAuthCallback);
+router.get("https://adelo.ai/dashboard/userprofile", googleAuthCallback);
 
 app.use("/api/v1", router);
 
